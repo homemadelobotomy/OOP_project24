adca2e0f3bc2453eef441a4f93c33c33acc8322f
Maxim <panshinmv@gmail.com>
2024-05-18 14:27:18 +0300

    task manager with gtk 4, begin

---
+++ task/task.h
@@ -0,0 +1,26 @@
+
+#include <gtk/gtk.h>
+
+static void
+print_hello (GtkWidget *widget,
+             gpointer   data)
+{
+  g_print ("Hello World\n");
+}
+
+class task
+{
+private:
+
+public:
+    task(/* args */);
+    ~task();
+};
+
+task::task(/* args */)
+{
+}
+
+task::~task()
+{
+}
